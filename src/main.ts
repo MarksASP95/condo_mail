a64c164d128dd12b27a139cbaa6381731f935499
Marco <marcosuarezp95@gmail.com>
2023-04-29 12:30:09 -0400

    start implementing form

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,37 @@ const sendMailInput: SendMailInput = {
   rateBs: 25,
 };
 
-// API.post("cmapi", "/send-mail", { body: sendMailInput})
-//   .then((response) => console.log("RESPONSE", response))
-//   .catch((err) => console.log("ERROR", err));
+const form = document.forms[0];
+form.addEventListener("submit", handleFormSubmit);
+
+const uploadingTextEl: HTMLParagraphElement = document.querySelector(".uploading-capture-text")!;
+
+function handleFormSubmit(e: SubmitEvent) {
+  e.preventDefault();
+  const amount = new FormData(e.target! as HTMLFormElement).get("amount");
+  const rate = new FormData(e.target! as HTMLFormElement).get("rate");
+  const monthtsStr = new FormData(e.target! as HTMLFormElement).get("months");
+  const file = new FormData(e.target! as HTMLFormElement).get("file");
+  console.log({ amount, rate, monthtsStr, file})
+
+  setShowUploadingText(true);
+  
+  return;
+  API.post(
+      "cmapi", 
+      "/send-mail", 
+      { 
+        body: sendMailInput, 
+        headers: {
+          "Authorization": `Basic ${import.meta.env.VITE_API_TOKEN}`
+        },
+      }
+    )
+      .then((response) => console.log("RESPONSE", response))
+      .catch((err) => console.log("ERROR", err));
+}
+
+function setShowUploadingText(show: boolean) {
+  if (show) return uploadingTextEl.classList.add("shown");
+  return uploadingTextEl.classList.remove("shown");
+}
