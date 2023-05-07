6bfcac53d52686e0c94fcfe05667517b5705b7ce
Marco <marcosuarezp95@gmail.com>
2023-05-06 20:37:03 -0400

    pwa config

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,13 @@
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 import path, { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   // appType: "mpa",
+  plugins: [
+    VitePWA({ registerType: 'autoUpdate' })
+  ],
   build: {
     rollupOptions: {
       input: {
